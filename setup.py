7944a97c59a32b6e9f3469ac352d906ea453ff48
Aashay30 <tamrakaraashay30@gmail.com>
2024-07-26 22:22:52 +0530

    adding setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+# to install local packages in our virtual environment
+
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenrator',
+    version='0.0.1',
+    author='Aashay Tamrakar',
+    author_email='tamrakaraashay30@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
